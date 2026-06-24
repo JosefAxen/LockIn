b06e845f16d695ff9e29f60d6d970969eab2fb1f
Josef Axén <josef.axen@impleo.se>
2026-06-24 17:34:25 +0200

    feat(i18n): add LocalizeExtension XAML markup extension

---
+++ LockIn/Resources/Strings/LocalizeExtension.cs
@@ -0,0 +1,23 @@
+using System.Globalization;
+
+namespace LockIn.Resources.Strings;
+
+/// <summary>
+/// XAML markup extension: <c>{loc:Localize Onboarding_Step0_Welcome}</c>
+/// Saknad nyckel returnerar nyckelnamnet bokstavligt så missar syns utan att krascha.
+/// </summary>
+[ContentProperty(nameof(Key))]
+public class LocalizeExtension : IMarkupExtension<string>
+{
+    public string Key { get; set; } = "";
+
+    public string ProvideValue(IServiceProvider serviceProvider)
+    {
+        if (string.IsNullOrEmpty(Key)) return string.Empty;
+        var culture = AppResources.Culture ?? CultureInfo.CurrentUICulture;
+        return AppResources.ResourceManager.GetString(Key, culture) ?? Key;
+    }
+
+    object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        => ProvideValue(serviceProvider);
+}
